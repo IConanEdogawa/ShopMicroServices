4491868749dffa08adb8a3ac551393bd0626e155
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-29 23:16:49 +0500

    fixed

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
+using Shop.Extensions;
 
 internal class Program
 {
@@ -8,10 +13,33 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
-
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer"
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+        });
 
+        builder.Services.AddJwtAuthentication(builder.Configuration); // Make sure this extension method is defined in your Shop.Extensions namespace
         builder.Services.AddCors(options =>
         {
             options.AddPolicy("AllowAnyOrigin",
@@ -23,7 +51,7 @@ internal class Program
                 });
         });
 
-        builder.Services.AddOcelot(builder.Configuration);
+        builder.Services.AddOcelot(builder.Configuration); // Make sure you've correctly configured Ocelot in your Shop.Extensions namespace
 
         var app = builder.Build();
 
@@ -38,7 +66,7 @@ internal class Program
             endpoints.MapControllers();
         });
 
-        app.UseOcelot().Wait();
+        app.UseOcelot().Wait(); // Use the Ocelot middleware to handle API Gateway functionality
 
         app.Run();
     }
--- src/Discount/Discount.Domain/Class1.cs
+++ src/Discount/Discount.Domain/Class1.cs
@@ -1,7 +1,9 @@
-﻿namespace Discount.Domain
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Discount.Domain
 {
     public class Class1
     {
-
+        public IFormFile image { get; set; }
     }
 }
